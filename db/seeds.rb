59febef30c8bdcaf6c173cc4c31331c7f8197a15
Brandon Weaver <a7xncob@gmail.com>
2020-01-30 19:28:07 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Test Artist")
+Song.create(title: "Test Song", artist_id: 1)
