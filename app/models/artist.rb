5e5e8a23867075b46830d796fe2d9f96a6d0a6ee
nkizito92 <nkizito57@gmail.com>
2020-02-10 14:11:02 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def artist_name=(artist)
+    self.artist = Artist.find_or_create_by(name: artist[:name])
+  end
+
+  def artist_name
+    self.artist ? self.category.name : nil
+  end
 end
